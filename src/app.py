d16db30752eac8e71e905a29e3b0b65a374ade24
joaonunez <joaovaldiglesias@gmail.com>
2024-08-15 01:12:10 -0400

    get all members ready on app.py

--- src/app.py
+++ src/app.py
@@ -26,17 +26,12 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_to_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
